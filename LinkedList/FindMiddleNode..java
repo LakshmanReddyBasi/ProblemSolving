0d982c3ed736dcfdd93074fd55767efa28c94f4e
LakshmanReddyBasi <22pa1a0514@vishnu.edu.in>
2024-12-23 23:26:06 +0530

    Create FindMiddleNode..java

---
+++ LinkedList/FindMiddleNode..java
@@ -0,0 +1,27 @@
+// Problem statement
+// Given a singly linked list of 'N' nodes. The objective is to determine the middle node of a singly linked list. However, if the list has an even number of nodes, we return the second middle node.
+
+// Note:
+// 1. If the list is empty, the function immediately returns None because there is no middle node to find.
+// 2. If the list has only one node, then the only node in the list is trivially the middle node, and the function returns that node.
+
+public class Solution
+{
+    public static Node findMiddle(Node head)
+    {
+        // Write your code here.
+        if(head == null) return null;
+        if(head.next == null) return head;
+        Node slow = head;
+        Node fast = head;
+        while(fast.next!=null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next;
+            if( fast.next!=null){
+                fast = fast.next;
+            }
+        }
+        if (fast.next==null) return slow;
+        else return slow.next;
+    }
+}
