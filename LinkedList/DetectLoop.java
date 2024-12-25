ffa88368278dbe4f9b39ca908e3dbe482179b7c9
LakshmanReddyBasi <22pa1a0514@vishnu.edu.in>
2024-12-25 17:46:38 +0530

    Create DetectLoop.java

---
+++ LinkedList/DetectLoop.java
@@ -0,0 +1,21 @@
+// Problem statement
+// You are given a Singly Linked List of integers. Return true if it has a cycle, else return false.
+// A cycle occurs when a node's next points back to a previous node in the list.
+// Example:
+// In the given linked list, there is a cycle, hence we return true
+
+
+public class Solution {
+    public static boolean detectCycle(Node head) {
+        //Your code goes here
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next ;
+            if(fast == slow )
+                return true;
+        }
+        return false;
+    }
+}
