66463a956051832cc1e6cefa3b2716a2d6c5dc12
LakshmanReddyBasi <22pa1a0514@vishnu.edu.in>
2025-02-08 23:52:39 +0530

    Create BoundaryTraversal.java

---
+++ GFGPOTD/trees/BoundaryTraversal.java
@@ -0,0 +1,61 @@
+class Solution {
+    static boolean isLeaf(Node node) {
+        return node.left == null && node.right == null;
+    }
+
+    static void collectBoundaryLeft(Node root, ArrayList<Integer> res) {
+        if (root == null || isLeaf(root))
+            return;
+
+        res.add(root.data);
+        if (root.left != null)
+            collectBoundaryLeft(root.left, res);
+        else if (root.right != null)
+            collectBoundaryLeft(root.right, res);
+    }
+
+    static void collectLeaves(Node root, ArrayList<Integer> res) {
+        if (root == null)
+            return;
+
+        if (isLeaf(root)) {
+            res.add(root.data);
+            return;
+        }
+
+        collectLeaves(root.left, res);
+        collectLeaves(root.right, res);
+    }
+
+
+    static void collectBoundaryRight(Node root, ArrayList<Integer> res) {
+        if (root == null || isLeaf(root))
+            return;
+
+        if (root.right != null)
+            collectBoundaryRight(root.right, res);
+        else if (root.left != null)
+            collectBoundaryRight(root.left, res);
+
+        res.add(root.data);
+    }
+    
+    static ArrayList<Integer> boundaryTraversal(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        if (root == null)
+            return res;
+
+        if (!isLeaf(root))
+            res.add(root.data);
+
+        collectBoundaryLeft(root.left, res);
+
+        collectLeaves(root, res);
+
+        collectBoundaryRight(root.right, res);
+
+        return res;
+    }
+
+}
